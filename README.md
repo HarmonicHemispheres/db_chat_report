4edb49f58501abc2bb1e998c5e038618d9c19918
harmonichemispheres <robby.boney@harmonichemispheres.com>
2024-01-23 15:23:47 -0800

    added files for project

--- README.md
+++ README.md
@@ -8,10 +8,13 @@ an interject report for creating and interacting with a database using natural l
 # Setup
 
 ### Add OpenAI API Key to Env Var
+> NOTE: Get your open ai api key ( https://platform.openai.com/api-keys )
 ```
 (windows)
 > Set-Item -Path Env:OPENAI_API_KEY -Value ($Env:OPENAI_API_KEY + "sk-???????????????????????????????")
 ```
+
+<br>
 
 ### Install Package
 NOTE: you may need to use your own package manager if default python is not >=3.11
@@ -21,7 +24,20 @@ NOTE: you may need to use your own package manager if default python is not >=3.
 > poetry install
 ```
 
+<br>
+
 ### Start Data API
 ```
 python data_api.py
 ```
+
+> NOTE: if you want to change the ai usage in this package, refer to openai's python package - https://pypi.org/project/openai/
+
+<br>
+
+### Create Interject Data Connection
+![Alt text](image.png)
+
+### Create Interject Data Portals
+![Alt text](image-1.png)
+![Alt text](image-2.png)
--- ids-python-api/data_api.py
+++ ids-python-api/data_api.py
@@ -4,22 +4,59 @@ from fastapi import FastAPI
 from openai import OpenAI
 import os
 import duckdb
+import uuid
+import datetime
 import json
 
-
-os.environ["OPENAI_API_KEY"] = "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"
+# [NOTE] use this to manually set your openai api key rather then reading from an environment variable  
+# os.environ["OPENAI_API_KEY"] = "sk-???????????????????????????????"
 
 API = FastAPI()
 
 # Set-Item -Path Env:OPENAI_API_KEY -Value ($Env:OPENAI_API_KEY + ";<new-value>")
-AI = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+AI = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"), )
 con = duckdb.connect("file.db")
 
+# ---------------------------- #
+# -------- GPT MODELS -------- #
+# ---------------------------- #
 # gpt-4-1106-preview
 # gpt-3.5-turbo-1106
 # gpt-3.5-turbo
 # gpt-4
-MODEL = "gpt-4-1106-preview"   
+MODEL = "gpt-4-1106-preview"
+def insert_logs(sid=None, status=None, tags=None, prompt=None, query="", created=None):
+
+    if not sid:
+        sid = str(uuid.uuid4())[:7]
+    
+    if not created:
+        created = datetime.datetime.now()
+
+    SQL = f"INSERT INTO logs (sid, created, status, tags, prompt, query) VALUES (?,?,?,?,?,?);"
+    con.execute(SQL, [sid, created, status, tags, prompt, query])
+
+@API.post("/get_prompts", response_model=InterjectResponse)
+async def get_prompts(request: InterjectRequest):
+
+    result = con.sql("SELECT prompt FROM (SELECT prompt, MAX(created) AS cr FROM logs GROUP BY prompt ORDER BY cr DESC LIMIT 20)")
+    print(result)
+
+    # --- get data
+    table: IDSTable = IDSTable(
+        TableName="Table",
+        Columns=[IDSColumn(ColumnName="Prompt", Ordinal=1),],
+        Rows=list(result.fetchall())
+    )
+
+    # create response
+    response: InterjectResponse = InterjectResponse(
+        RequestParameterList=request.RequestParameterList,
+        ReturnedDataList=[
+            ReturnedData(data=table).to_dict()
+        ]
+    )
+    return response
 
 
 @API.post("/the_one_call", response_model=InterjectResponse)
@@ -35,6 +72,7 @@ async def the_one_call(request: InterjectRequest):
     try:
         # --- process request for actions
         prompt: str = request.get_param("prompt").InputValue
+    
         print("PROMPT:   ", prompt)
             
         SYS_MESSAGE = """
@@ -68,6 +106,7 @@ TABLES:
             ],
             model=MODEL,
             response_format={"type": "json_object"},
+            # temperature=0
         )
 
         sql = chat_completion.choices[0].message.content
@@ -99,6 +138,7 @@ TABLES:
             print("R:   ", rows)
 
             sql = f"READ -- {sql_dict['READ']}"
+            insert_logs(status="INFO", tags="READ", prompt=prompt, query=sql_dict['READ'])
 
 
         ##############################
@@ -107,10 +147,11 @@ TABLES:
         elif "WRITE" in sql_dict:
             result = con.sql(sql_dict['WRITE'])
             sql = f"WRITE -- {sql_dict['WRITE']}"
-
+            insert_logs(status="INFO", tags="WRITE", prompt=prompt, query=sql_dict['WRITE'])
 
     except Exception as error:
         sql = f"ERROR -- {error}"
+        insert_logs(status="ERROR",tags="",prompt=str(error))
     
     # --- get data
     table: IDSTable = IDSTable(
@@ -138,7 +179,12 @@ TABLES:
 if __name__ == '__main__':
     cfg = Config()
     cfg.load()
-    
+
+    # ---- SETUP DB ---- #
+    SQL = "CREATE TABLE IF NOT EXISTS logs (sid VARCHAR, created TIMESTAMP, status VARCHAR, tags VARCHAR, prompt VARCHAR, query VARCHAR);"
+    con.sql(SQL)
+
+
     uvicorn_config = uvicorn.Config("data_api:API", 
                                     host=cfg.host,
                                     port=cfg.port, 
